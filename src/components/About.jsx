cbae7cc862b8731a8b590bb0319c104e55a66d43
Emanuel Martinez <yinproducciones@gmail.com>
2024-01-06 18:12:06 -0300

    add: fix responsive styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,13 @@ const About = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-24">
           <img
-            className="object-cover h-[500px] w-[566px] md:mx-auto lg:mx-0 rounded-2xl"
+            className="object-cover h-[500px] w-[566px] md:mx-auto lg:mx-auto rounded-2xl"
             src={Image}
             alt="/"
           />
           <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
             <div className="flex flex-col">
-              <h2 className="text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block">
+              <h2 className="flex justify-center text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3 before:content-about relative before:absolute before:opacity-40 before:-top-[2rem] before:hidden before:lg:block">
                 Emanuel Martinez
               </h2>
               <p className="mb-4 text-accent">Frontend Developer</p>
--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import { brands } from "../data";
 
 const Brands = () => {
   return (
-    <section className="min-h-[200px] bg-tertiary flex items-center ">
-      <div className="container mx-auto flex md:justify-center items-center flex-wrap justify-evenly">
-        {/* {brands.map((brand, index) => {
-          return (
-            <div key={index}>
-              <img src={brand.img} alt="/" />
-            </div>
-          );
-        })} */}
-          <p className='section-title before:content-portfolio relative before:absolute before:opacity-40 before:-top-[2rem] before:-left-3/4 before:hidden before:lg:block'>Frontend developer</p>
+    <section className="min-h-[200px] bg-tertiary flex items-center justify-center">
+      <div className="container mx-auto flex items-center flex-wrap justify-center">
+          <p className='section-title before:content-portfolio relative before:absolute before:opacity-40 before:-top-[2rem] justify-center flex before:hidden before:lg:block'>Frontend developer</p>
       </div>
     </section>
   );
